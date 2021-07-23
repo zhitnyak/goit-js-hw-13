5e454d404ab5190e628c415eae8e0e10de2c5cd9
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-07-23 22:41:54 +0300

    failure_litebox(((

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,3 +1,3 @@
 import '../../node_modules/simplelightbox/dist/simple-lightbox.css';
 import SimpleLightbox from 'simplelightbox';
-let gallery = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.gallery a');
